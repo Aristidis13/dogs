ee9151d1b9951f108ea4c22c683591c21986151a
Aristidis13 <aris13pat@hotmail.com>
2022-02-17 14:48:46 +0200

    Made the API Call run smoothly

--- src/Breeds/breeds.tsx
+++ src/Breeds/breeds.tsx
@@ -7,43 +7,22 @@ interface BreedsPageProps {
 
 type TBreedType = {
     name: string,
-    img: string | void,
     subBreeds:string[]
-}
-
-const getUrl = async ( breeds:TBreedType[]) => {
-    for (let i=0; i<breeds.length; i++) {
-        await axios
-        .get('breed/'+breeds[i].name+'/images/random')
-        .then( res => {breeds[i].img = res.data.message; })
-        .catch(err => {console.error('Error in image fetching: '+err);})
-    }
-    return breeds;
 }
 
 const BreedsPage: FunctionComponent<BreedsPageProps | undefined> = () => {
     const [breedsContainer,setBreeds] = useState([]);
-    let breeds:TBreedType[] = [];
     useEffect( () => {
-        let time1 = performance.now()
+        let breeds:TBreedType[] = [];
         axios
         .get('breeds/list/all')
-        .then( async list => {
+        .then( list => {
             Object.entries(list.data.message).forEach( someBreed => {
-                let thisbreed = { "name": someBreed[0], "img" : "", "subBreeds": someBreed[1] as string[] }
+                let thisbreed = { "name": someBreed[0], "subBreeds": someBreed[1] as string[] }
                 breeds.push(thisbreed);
             })
-        let time2 = performance.now();
-            console.group("Time passed ");
-            console.log(time2-time1);
-            console.groupEnd();
-            await getUrl(breeds).then((breeds) =>{setBreeds(breeds as never[]);}).catch(error=>console.log(error));
-            let time3 = performance.now();
-            console.group("Time passed ");
-            console.log(time3-time2);
-            console.groupEnd();
+            setBreeds(breeds as never[])
         }).catch(error => console.log("Error in fetching main breeds: " + error));
-
     },[]);
     return <section>
         <h2>This is the Breeds Page</h2>
@@ -51,7 +30,6 @@ const BreedsPage: FunctionComponent<BreedsPageProps | undefined> = () => {
              breedsContainer.map( breed => <MainBreed
                     key={breed['name']}
                     name={breed['name']}
-                    imageUrl={breed['img']}
                     subBreeds={ breed['subBreeds']}
                 />
                )
@@ -62,14 +40,21 @@ const BreedsPage: FunctionComponent<BreedsPageProps | undefined> = () => {
 
 interface MainBreedProps {
     name:string;
-    imageUrl:string; 
+    //imageUrl:string; <Link className="rater" > </Link>
     subBreeds: string[];
 }
 
 const MainBreed: FunctionComponent<MainBreedProps> = (props:MainBreedProps) => {
+    const [imgUrl,setBreedImg] = useState("");
+    useEffect( () => {
+        axios
+        .get('breed/'+props.name+'/images/random')
+        .then( res => {setBreedImg(res.data.message); })
+        .catch(err => {console.error('Error in image fetching: '+err);})
+    },[imgUrl,props.name])
     return  <figure key={props.name}>
         <figcaption className="breed-header">{props.name}</figcaption>
-        <img className="breed-image" src={props.imageUrl} alt={"Image of "+props.name} />
+            <img className="breed-image" src={imgUrl} alt={"Image of "+props.name} />
     </figure>
 }
 
