cdc40b08ec3c23a4fcb7b5357bfe51161d40752e
Aristidis13 <aris13pat@hotmail.com>
2022-02-17 03:20:51 +0200

    Made the API Calls but late - Needs restructuring

--- src/Breeds/breeds.tsx
+++ src/Breeds/breeds.tsx
@@ -7,35 +7,43 @@ interface BreedsPageProps {
 
 type TBreedType = {
     name: string,
-    img: string,
+    img: string | void,
     subBreeds:string[]
 }
 
-const BreedsPage: FunctionComponent<BreedsPageProps> = () => {
+const getUrl = async ( breeds:TBreedType[]) => {
+    for (let i=0; i<breeds.length; i++) {
+        await axios
+        .get('breed/'+breeds[i].name+'/images/random')
+        .then( res => {breeds[i].img = res.data.message; })
+        .catch(err => {console.error('Error in image fetching: '+err);})
+    }
+    return breeds;
+}
+
+const BreedsPage: FunctionComponent<BreedsPageProps | undefined> = () => {
     const [breedsContainer,setBreeds] = useState([]);
+    let breeds:TBreedType[] = [];
     useEffect( () => {
-        let breeds:TBreedType[] = [];
+        let time1 = performance.now()
         axios
         .get('breeds/list/all')
-        .then( someBreed => {
-            Object.entries(someBreed.data.message).forEach( breed => {
-            let breedObj:TBreedType = {
-                "name": breed[0],
-                "img" : "",
-                "subBreeds": breed[1] as string[]
-                }
-                axios
-                .get('breed/'+breed[0]+'/images/random')
-                .then( async res => {  breedObj.img= await res.data.message; })
-                .catch(err => {console.error('Error in image fetching: '+err);})
-                breeds.push(breedObj);
-                console.log(breedObj);
-                console.log(breeds)
-                
-            })}).then(() => {
-                setBreeds(breeds as never[]);
-    }).catch(error => console.log("Error in fetching main breeds: " + error));
-    
+        .then( async list => {
+            Object.entries(list.data.message).forEach( someBreed => {
+                let thisbreed = { "name": someBreed[0], "img" : "", "subBreeds": someBreed[1] as string[] }
+                breeds.push(thisbreed);
+            })
+        let time2 = performance.now();
+            console.group("Time passed ");
+            console.log(time2-time1);
+            console.groupEnd();
+            await getUrl(breeds).then((breeds) =>{setBreeds(breeds as never[]);}).catch(error=>console.log(error));
+            let time3 = performance.now();
+            console.group("Time passed ");
+            console.log(time3-time2);
+            console.groupEnd();
+        }).catch(error => console.log("Error in fetching main breeds: " + error));
+
     },[]);
     return <section>
         <h2>This is the Breeds Page</h2>
